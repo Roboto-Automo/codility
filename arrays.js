1016485c24602801bb6ba4d04bf511ad6dd83864
Automoroboto <roberto@studiodespirito.co.uk>
2024-03-28 10:41:41 +0000

    finding unpaired element in array

--- arrays.js
+++ arrays.js
@@ -16,3 +16,31 @@ array.unshift(popValue)// adds the removed element to the beginning of the array
 return array;
 
 }
+
+
+//Finding the unpaired element in an array
+
+function solution(A) {
+    let unpaired = 0;
+    for (let i = 0; i < A.length; i++) {
+        unpaired ^= A[i];
+    }
+    return unpaired;
+}
+
+
+function solution(A) {
+    // Implement your solution here
+
+   A.sort();
+
+    // Iterate through the sorted array in steps of 2
+    for (let i = 0; i < A.length; i += 2) {
+        // Check if the current element and the next one are different
+        if (A[i] !== A[i + 1]) {
+            // If they are different, the current element is unpaired
+            return A[i];
+        }
+    }
+
+}
