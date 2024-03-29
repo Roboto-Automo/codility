900bc72205e129746fe88f676d16a1b377f64506
Automoroboto <roberto@studiodespirito.co.uk>
2024-03-29 17:06:57 +0000

    solving leetcode question properly

---
+++ removingValInPlace.js
@@ -0,0 +1,26 @@
+//leetcode 27. Remove Element
+
+//this is a handy algorithm to remove elements in place from an array as the loop runs
+
+var removeElement = function(nums, val) {
+
+    let k = 0 // keeps track of the number of elements not equal to val, doesn't increment if element is equal to val
+
+       for (let i = nums.length-1; i >=0; i--) {
+            if (nums[i] === val) {
+                nums.splice(i,1) //removes the element at index i
+            }else {
+                k++
+            }
+           
+        }
+       
+     console.log(k);
+    return nums;
+};
+
+console.log(removeElement([3,2,2,3,7,5],3)) 
+
+//THINGS TO REMEMBER
+
+//count backwards when removing elements in place from an array
