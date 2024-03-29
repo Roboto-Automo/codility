eecd75966dd4304e626c394426b79be1676afb2b
Automoroboto <roberto@studiodespirito.co.uk>
2024-03-29 12:23:54 +0000

    password checker codility

--- arrays.js
+++ arrays.js
@@ -1,46 +1,50 @@
 //rotation of array
 function solution(A, K) {
-  
+  let array = A;
+  let popValue;
 
-    let array = A;
-    let popValue;
-
-if (array.length === 0){
+  if (array.length === 0) {
     return [];
-}
+  }
 
-for(let i = 0; i < K;i++){
-popValue= array.pop() //removes last element in array and returns it and stores in variable
-array.unshift(popValue)// adds the removed element to the beginning of the array
+  for (let i = 0; i < K; i++) {
+    popValue = array.pop(); //removes last element in array and returns it and stores in variable
+    array.unshift(popValue); // adds the removed element to the beginning of the array
+  }
+  return array;
 }
-return array;
-
-}
-
 
 //Finding the unpaired element in an array
 
 function solution(A) {
-    let unpaired = 0;
-    for (let i = 0; i < A.length; i++) {
-        unpaired ^= A[i];
-    }
-    return unpaired;
+  let unpaired = 0;
+  for (let i = 0; i < A.length; i++) {
+    unpaired ^= A[i];
+  }
+  return unpaired;
 }
-
 
 function solution(A) {
-    // Implement your solution here
+  // Implement your solution here
 
-   A.sort();
+  A.sort();
 
-    // Iterate through the sorted array in steps of 2
-    for (let i = 0; i < A.length; i += 2) {
-        // Check if the current element and the next one are different
-        if (A[i] !== A[i + 1]) {
-            // If they are different, the current element is unpaired
-            return A[i];
-        }
+  // Iterate through the sorted array in steps of 2
+  for (let i = 0; i < A.length; i += 2) {
+    // Check if the current element and the next one are different
+    if (A[i] !== A[i + 1]) {
+      // If they are different, the current element is unpaired
+      return A[i];
     }
-
+  }
 }
+
+function Filter(array) {
+  newArray = [];
+
+  newArray = array.filter((element, index, array) => element === index);
+
+  return newArray;
+}
+
+console.log(Filter([0, 2, 3, 3, 5, 1, 1, 8, 9, 9]));
---
+++ passwordchecker.js
@@ -0,0 +1,47 @@
+
+function isAlphanumeric(str) {
+    return /^[a-zA-Z0-9]+$/.test(str);
+  }
+  function isLetter(char) {
+    return /[a-zA-Z]/.test(char);
+  }
+  
+  function longestStringElement(array) {
+      return array.reduce((longest, current) => current.length > longest.length ? current : longest, "");
+  }
+  
+  function solution(S) {
+  let longestLength = -1
+  let newArray =[]
+  let array = S.split(" ")
+  let letterCount = 0;
+  let totalCount = 0;
+  let alphanumeric = true;
+  
+  for (let i = 0; i < array.length;i++){
+       letterCount = 0;
+        totalCount = 0;
+        alphanumeric = true;
+  for(let j = 0; j < array[i].length;j++){
+      totalCount++
+  if(!isAlphanumeric(array[i][j])){
+  alphanumeric = false;
+  }
+  if(isLetter(array[i][j])){
+      letterCount++
+  }
+  }
+  if (letterCount % 2 === 0 && totalCount % 2 === 1 && alphanumeric === true){
+      newArray.push(array[i])
+  }
+  
+  
+  }
+  
+  if(newArray.length !== 0){
+  longestLength = longestStringElement(newArray).length
+  
+  return longestLength
+  }else {return longestLength}
+  
+  }
