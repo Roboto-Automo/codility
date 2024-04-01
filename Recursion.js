db40007bedd8415967ae3e81fd31b880be70f077
Automoroboto <roberto@studiodespirito.co.uk>
2024-04-01 21:26:01 +0100

    simple recursion example

--- Recursion.js
+++ Recursion.js
@@ -12,9 +12,17 @@ function combinations(arr, k) {
     }
 
     for (let i = start; i < arr.length; i++) {
-        innerFunction(combo.concat(arr[i]), i + 1);
-        console.log(combo, "current value",arr[i],"loop index", i, "current start",start);
-        // console.log(mainCombsArray);
+      innerFunction(combo.concat(arr[i]), i + 1);
+      console.log(
+        combo,
+        "current value",
+        arr[i],
+        "loop index",
+        i,
+        "current start",
+        start
+      );
+      // console.log(mainCombsArray);
     }
   }
 
@@ -23,3 +31,23 @@ function combinations(arr, k) {
 }
 
 console.log(combinations([1, 2, 3], 2));
+
+function getDiff(startNum, endNum) {
+  // Base case: if the difference between endNum and startNum is 2,
+  // return an array containing the number between them.
+  if (endNum - startNum === 2) {
+    return [startNum + 1];
+  } else {
+    // Recursive case: generate the range between startNum and endNum - 1,
+    // then push the current endNum - 1 to the array.
+    let list = getDiff(startNum, endNum - 1);
+    list.push(endNum - 1);
+    return list;
+  }
+}
+
+console.log(getDiff(1, 9), "simple recursion");
+
+//THINGS TO REMEMBER
+//recursive functions need a base case and a recursive case
+//recursive functions should be able to return a value
